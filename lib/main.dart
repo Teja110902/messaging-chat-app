0b5f6b3eb6783860ad7d65474d66fa95d37782a4
Teja110902 <74037779+Teja110902@users.noreply.github.com>
2023-04-25 21:29:31 +0530

    Deleted unnecessary commits

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_ui/features/landing/screens/landing_screen.dart';
+import 'package:whatsapp_ui/firebase_options.dart';
+import 'package:whatsapp_ui/responsive/responsive_layout.dart';
+import 'package:whatsapp_ui/router.dart';
+import 'package:whatsapp_ui/screens/mobile_screen_layout.dart';
+import 'package:whatsapp_ui/screens/web_screen_layout.dart';
+import 'colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Binds framework to Flutter engine
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp UI',
+      theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: backgroundColor,
+          appBarTheme: const AppBarTheme(
+            color: appBarColor,
+          )),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const LandingScreen(),
+    );
+  }
+}
