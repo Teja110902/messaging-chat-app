70e12adfba3b742a3bfb33c88370ab82da85fc15
Teja110902 <74037779+Teja110902@users.noreply.github.com>
2023-04-26 18:55:22 +0530

    Added persistent authentication functionality

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -4,12 +4,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_ui/features/auth/repository/auth_repository.dart';
 
+import '../../../models/user_model.dart';
+
 final authControllerProvider = Provider(
   (ref) {
     final authRepository = ref.watch(authRepositoryProvider);
     return AuthController(authRepository: authRepository, ref: ref);
   },
 );
+
+final userDataAuthProvider = FutureProvider((ref) {
+  final authController = ref.watch(authControllerProvider);
+
+  return authController.getUserData();
+});
 
 class AuthController {
   final AuthRepository authRepository;
@@ -19,6 +27,12 @@ class AuthController {
     required this.authRepository,
     required this.ref,
   });
+
+  Future<UserModel?> getUserData() async {
+    UserModel? user = await authRepository.getCurrentUserData();
+
+    return user;
+  }
 
   void signInWithPhone(BuildContext context, String phoneNumber) {
     authRepository.signInWithPhone(context, phoneNumber);
